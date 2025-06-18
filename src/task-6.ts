ce62c5ab266f5faf2a57be5be9b3cfdd211f47e8
Sergii Gorodeikin <55316965+iEnchanter@users.noreply.github.com>
2025-06-18 22:50:58 +0200

    upgrd

---
+++ src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+}
+
+const firstNumber = getFirstElement<number>([1, 2, 3]);
+const firstString = getFirstElement<string>(["a", "b", "c"]);
+const firstBoolean = getFirstElement<boolean>([true, false, true]);
+
+const mixed = getFirstElement<number | string | boolean>([1, "a", true]);
